4423e40d9df518d250e79258dded997ddf73ed6a
PardeepDeveloperIIOT <pardeepnain1111@gmail.com>
2026-01-25 19:08:58 +0100

    connect to the local database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,25 @@
 import app from "./app.js";
+import { mongodbConnection } from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-const server = app.listen(3000, () => {
+//database Connection
+mongodbConnection();
+
+// ⭕ refrence error:uncaughtException error handle
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION 💥 Shutting down...", err.name, err.message);
+  process.exit(1);
+});
+
+const server = app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000");
+});
+
+// ⭕ promiss error handle (catch)
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED PROMISE REJECTION 💥 Shutting down...", err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
